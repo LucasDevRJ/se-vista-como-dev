9787ad571076c4bfe2057f3699b20050807f8da2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-06 13:17:29 -0300

    instanciando camisetas

--- src/com/github/lucasdevrj/sevistacomodev/principal/MenuCamiseta.java
+++ src/com/github/lucasdevrj/sevistacomodev/principal/MenuCamiseta.java
@@ -8,6 +8,12 @@ public class MenuCamiseta {
     Camiseta camiseta1 = new Camiseta("Camiseta Relaxed", 99.90, "Branca",
             'M', "Blue Steel",
             "Camiseta Relaxed Em Algodão Com Gola Média E Manga Curta Branco");
+    Camiseta camiseta2 = new Camiseta("Camiseta Regular", 59.90, "Preta",
+            'G', "Ripping",
+            "Camiseta Regular Em Algodão Com Estampa De Triângulo E Lettering Preto");
+    Camiseta camiseta3 = new Camiseta("Camiseta Regular", 69.90,
+            "Marrom", 'M', "Ripping",
+            "Camiseta Regular Básica Em Algodão Com Manga Longa Marrom");
     public static void main(String[] args) {
         MenuCamiseta menuCamiseta = new MenuCamiseta();
         menuCamiseta.exibeMenuCamiseta();
@@ -20,11 +26,11 @@ public class MenuCamiseta {
                 -------------------|CAMISETAS|-------------------
                 Nossas Camisetas:
                 1 - Ver %s
-                2 - Ver 
-                3 - Ver 
+                2 - Ver %s
+                3 - Ver %s
                 4 - Voltar
                 --------------------------------------------
-                """.formatted(camiseta1.getNome());
+                """.formatted(camiseta1.getNome(), camiseta2.getNome(), camiseta3.getNome());
         System.out.println(menu);
         System.out.print("Digite a opção desejada: ");
         int opcao = entrada.nextInt();
