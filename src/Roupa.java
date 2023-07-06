8e172e95b4be5c05c3263c348bd72c1040099f22
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 21:19:30 -0300

    atributos da Roupa

--- src/Roupa.java
+++ src/Roupa.java
@@ -1,2 +1,8 @@
 public class Roupa {
+    private String nome;
+    private double preco;
+    private String cor;
+    private String tamanho;
+    private String marca;
+    private String descricao;
 }
