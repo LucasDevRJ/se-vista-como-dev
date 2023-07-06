3182df77b75a5d551c1aa6e983b1fe254b53731c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 21:29:20 -0300

    construtores

--- src/Camiseta.java
+++ src/Camiseta.java
@@ -1,2 +1,6 @@
 public class Camiseta extends Roupa {
+    public Camiseta(String nome, double preco, String cor,
+                    String tamanho, String marca, String descricao) {
+        super(nome, preco, cor, tamanho, marca, descricao);
+    }
 }
--- src/Roupa.java
+++ src/Roupa.java
@@ -1,8 +1,18 @@
 public class Roupa {
-    private String nome;
-    private double preco;
-    private String cor;
-    private String tamanho;
-    private String marca;
-    private String descricao;
+    protected String nome;
+    protected double preco;
+    protected String cor;
+    protected String tamanho;
+    protected String marca;
+    protected String descricao;
+
+    public Roupa(String nome, double preco, String cor,
+                 String tamanho, String marca, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.marca = marca;
+        this.descricao = descricao;
+    }
 }
