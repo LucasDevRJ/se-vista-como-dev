84ab150ffd751789756149f41418fb04fbb439fc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 21:36:20 -0300

    criação da classe filha Calca

---
+++ src/com/github/lucasdevrj/sevistacomodev/modelos/Calca.java
@@ -0,0 +1,7 @@
+package com.github.lucasdevrj.sevistacomodev.modelos;
+
+public class Calca extends Roupa {
+    public Calca(String nome, double preco, String cor, String tamanho, String marca, String descricao) {
+        super(nome, preco, cor, tamanho, marca, descricao);
+    }
+}
--- src/com/github/lucasdevrj/sevistacomodev/principal/Principal.java
+++ src/com/github/lucasdevrj/sevistacomodev/principal/Principal.java
@@ -1,4 +1,5 @@
 package com.github.lucasdevrj.sevistacomodev.principal;
 
 public class Principal {
+
 }
