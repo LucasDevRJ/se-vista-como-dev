ad75be05f503ce927ec3b417dd4ee0caafebe27d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 21:42:20 -0300

    crição do menu interativo

--- src/com/github/lucasdevrj/sevistacomodev/principal/Principal.java
+++ src/com/github/lucasdevrj/sevistacomodev/principal/Principal.java
@@ -1,5 +1,21 @@
 package com.github.lucasdevrj.sevistacomodev.principal;
 
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String menu = """
+                Bem-vindo(a) a loja Se Vista como Dev!
+                -------------------|MENU|-------------------
+                Nossas Vestimentas:
+                1 - Camisetas
+                2 - Calças
+                --------------------------------------------
+                """;
+        System.out.println(menu);
+        System.out.print("Digite a opção desejada: ");
+        int opcao = entrada.nextInt();
+    }
 }
